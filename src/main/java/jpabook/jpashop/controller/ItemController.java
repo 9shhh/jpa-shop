fd64ec467b119f86c87fc1865e02391d23d2d9d3
9shhh <uelj1234@naver.com>
2023-01-11 01:36:10 +0900

    상품 등록 개발

---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        Book book = new Book();
+
+        // 현재 코드는 setter 를 통한 값 셋팅이지만, Order 엔티티의 createOrder 와 같은 생성 메소드를 만들어서 처리하는게 바람직한 방법임.
+        // 실무에서는 static 생성자(생성 메소드)로 처리하자.
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+
+    }
+}
